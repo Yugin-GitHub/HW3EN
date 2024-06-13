775458042024712fdfe4bf36ace8fd815e16ed03
Евгений Волков <evgenijvolkov@MacBook-Pro-16.local>
2024-06-13 16:00:40 +0300

    Добавил пустой проект в Git

---
+++ HW-3 Evgeniy Nesterenko/HW-3 Evgeniy Nesterenko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-3 Evgeniy Nesterenko
+//
+//  Created by Евгений Волков on 13.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
